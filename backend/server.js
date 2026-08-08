368a7bd3a1b2505ef551f8e8e2e054c0b56f7609
Abderazek <abderazekbelhadjmbarek@gmail.com>
2026-08-08 16:40:23 +0100

    vercel

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,27 @@ const seedAdmin = require("./seedAdmin");
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const propertyRoutes = require("./routes/propertyRoutes");
-const userRoutes = require('./routes/userRoutes');
-const adminClientRoutes = require('./routes/adminClientRoutes');
-const enquiryRoutes = require('./routes/enquiryRoutes');
+const userRoutes = require("./routes/userRoutes");
+const adminClientRoutes = require("./routes/adminClientRoutes");
+const enquiryRoutes = require("./routes/enquiryRoutes");
 
 // Load other env vars (PORT, MONGO_URI, JWT_SECRET) from .env
 require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://estate-pro-henna.vercel.app",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/properties", propertyRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/admin/clients', adminClientRoutes);
-app.use('/api/enquiries', enquiryRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/admin/clients", adminClientRoutes);
+app.use("/api/enquiries", enquiryRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
